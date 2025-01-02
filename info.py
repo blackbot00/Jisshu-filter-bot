f3e93a1a30cd71d81b28243ffe6cbb0f2f75c79c
blackbot00 <109160013+blackbot00@users.noreply.github.com>
2025-01-02 19:28:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviehubsearchg
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001692857281').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', ""mongodb+srv://worm2807:D615k1OLJjP1bfzG@cluster0.mtp42.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://worm2807:D615k1OLJjP1bfzG@cluster0.mtp42.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 #---------------------------------------------------------------
